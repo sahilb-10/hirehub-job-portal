2386f14a2d0c6af74655fbfce8df13f67694051d
Sahil Bhandari <sahilengg7@gamil.com>
2026-05-06 19:35:03 +0530

    add error handling for database connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try{
         const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log("MongoDB COnnected");
+        console.log("MongoDB Connected");
+    } catch (error){
+        console.error(error);
+        process.exit(1);
     }
 };
