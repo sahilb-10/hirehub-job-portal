43301f16b8da511ab8c29fa347e70da633277dc4
Sahil Bhandari <sahilengg7@gamil.com>
2026-05-06 23:04:25 +0530

    fix middleware and express initialization order in server setup

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
+const express = require("express");
 const dotenv = require("dotenv");
-dotenv.config();
+const cors = require("cors");
 
 const connectDB = require("./config/db");
-const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-const express = require("express");
-
-const app = express();
+connectDB();
 
 app.get("/", (req,res) => {
     res.send("API is running...");
 });
 
 const PORT = process.env.PORT || 5000;
-
-connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
