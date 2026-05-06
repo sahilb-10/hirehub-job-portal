fe79b86d7c7ae6c8117913997e59118da0783f37
Sahil Bhandari <sahilengg7@gamil.com>
2026-05-06 18:51:15 +0530

    move port configuration to environment variables

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.get("/", (req,res) => {
     res.send("API is running...");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
