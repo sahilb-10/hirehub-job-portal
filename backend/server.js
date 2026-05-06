a3799f5c096162ec25ba09f74339cce9429b9c13
Sahil Bhandari <sahilengg7@gamil.com>
2026-05-06 19:17:27 +0530

    add middleware for cors and json parsing

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const cors = require("cors");
- 
+app.use(cors());
+app.use(express.json());
+
 const express = require("express");
 
 const app = express();
