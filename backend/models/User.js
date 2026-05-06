fb0efa1e60e95c1f7234d246b58f19062c078304
Sahil Bhandari <sahilengg7@gamil.com>
2026-05-06 23:19:28 +0530

    complete user schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,4 +4,13 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    }
 });
