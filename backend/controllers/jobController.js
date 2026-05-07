56e30312f6482c26489558cdff09dc1dba9bb1ef
Sahil Bhandari <sahilengg7@gamil.com>
2026-05-07 16:18:24 +0530

    added error handling and added database and sent succesful response

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -1,1 +1,10 @@
 const Job = require("../models/Jobs");
+
+const createJob = async (req, res) => {
+    try {
+        const job = await Job.create(req.body);
+        res.status(201).json(job);
+    }catch(error){
+
+    }
+};
