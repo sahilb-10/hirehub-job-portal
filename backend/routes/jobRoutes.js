0f4d99c024292decf086c2015df98767b5e71a9e
Sahil Bhandari <sahilengg7@gamil.com>
2026-05-08 23:25:45 +0530

    added get single job api

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { createJob, getJobs } = require("../controllers/jobController");
+const { createJob, getJobs, getJobById, } = require("../controllers/jobController");
 router.post("/", createJob);
 router.get("/", getJobs);
+router.get("/:id", getJobById);
 
 module.exports = router;
