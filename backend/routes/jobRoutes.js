7aa712962dde471554ef394bb451d518ed542584
Sahil Bhandari <sahilengg7@gamil.com>
2026-05-08 19:30:41 +0530

    add post route for creating jobs

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 const { createJob } = require("../controllers/jobController");
+router.post("/", createJob);
 
 module.exports = router;
