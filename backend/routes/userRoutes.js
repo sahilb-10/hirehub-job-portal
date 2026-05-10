4438640b58974567bbe4f80def1e0bfdf246baef
Sahil Bhandari <sahilengg7@gamil.com>
2026-05-10 17:54:23 +0530

    added register route for users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const { regiterUser } = require("../controllers/userController");
+
+router.post("/register", registerUser);
+
 module.exports = router;
