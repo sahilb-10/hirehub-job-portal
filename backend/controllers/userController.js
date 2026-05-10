bf7d2b1da77cb0dcef26a49acae665c6fbfd16e7
Sahil Bhandari <sahilengg7@gamil.com>
2026-05-10 17:45:02 +0530

    added user registration logic with password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,21 @@ const registerUser = async (req, res) =>{
     try{
         const { name, email, password } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+        res(201).json({
+            message: "User Registered Succesfully",
+        });
     }catch(error){
-
+        res.status(500).json({
+            message: error.message,
+        });
     }
+};
+
+module.exports = {
+    registerUser,
 };
