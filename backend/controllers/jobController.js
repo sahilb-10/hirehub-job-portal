309b3e41ef94c520ea055804deaf555864ea21ba
Sahil Bhandari <sahilengg7@gamil.com>
2026-05-10 15:34:12 +0530

    added delete job controller

--- backend/controllers/jobController.js
+++ backend/controllers/jobController.js
@@ -47,9 +47,23 @@ const updateJob = async (req, res) =>{
         });
     }
 };
+
+const deleteJob = async (req, res) =>{
+    try{
+        await Job.findByIdAndDelete(req.params.id);
+        res.status(200).json({
+            message: "Job deleted Successfully",
+        });
+    }catch(error){
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
 module.exports = {
     createJob,
     getJobs,
     getJobById,
     updateJob,
+    deleteJob,
 };
