cb0e7d9d3091c2288265bcece1d3cb3c437d99dd
Sahil Bhandari <sahilengg7@gamil.com>
2026-05-10 16:01:42 +0530

    added delete job api route

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { createJob, getJobs, getJobById, updateJob } = require("../controllers/jobController");
+const { createJob, getJobs, getJobById, updateJob, deleteJob } = require("../controllers/jobController");
 router.post("/", createJob);
 router.get("/", getJobs);
 router.get("/:id", getJobById);
 router.put("/:id", updateJob);
+router.delete("/:id", deleteJob);
 
 module.exports = router;
